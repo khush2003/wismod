38e6ab4077c32596822119798964fa76547fa787
Khush Agarwal <khush.agar@kmutt.ac.th>
2023-04-26 16:56:03 +0700

    Adding Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:wismod/firebase_options.dart';
 import 'package:wismod/routes/routes.dart';
+import 'package:wismod/theme/theme_data.dart';
 
-void  main() async {
+void main() async {
   // Initializing Firebase
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
@@ -18,9 +19,11 @@ class Application extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'WisMod',
+      theme: AppThemeData.themedata,
       //TODO: Implement Function to route user to login screen if it is not his first time on the app.
       //TODO: Add global/app-wide theme (Stored in theme folder)
-      initialRoute: Routes.onboarding, // Goes to Onboarding Page Initially (We will be using named routes, routes stored in routes.dart)
+      initialRoute: Routes
+          .onboarding, // Goes to Onboarding Page Initially (We will be using named routes, routes stored in routes.dart)
       getPages: getPages,
     );
   }

18627ffc35028ab37d23dc821766a3145f7231f1
ImUlyssus <98200468+ImUlyssus@users.noreply.github.com>
2023-04-27 01:18:35 +0700

    Onboarding draft
    onboarding page draft completed

--- lib/modules/home/views/onboarding.dart
+++ lib/modules/home/views/onboarding.dart
@@ -11,7 +11,79 @@ class OnboardingView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Wrap(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0.0, 60.0, 0.0, 40.0),
+                  child: Text(
+                    'Welcome to',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.displayLarge,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.network(
+                  'https://drive.google.com/file/d/1iVtDq_M5nygA6tqZxDR9yrBMWX1noPff/view',
+                  height: 200,
+                ),
+              ],
+            ),
+            const SizedBox(height: 50),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(8.0, 90.0, 8.0, 90.0),
+              child: Text(
+                'Join the community of KMUTT changemakers with WisMod',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+            const SizedBox(height: 50),
+            PrimaryButtonMedium(
+              onPressed: () => Get.toNamed(Routes.login),
+              child: const Text('Log In'),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Doesn't have an account? ",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 12),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Get.toNamed('/signup');
+                  },
+                  child: const MouseRegion(
+                    cursor: SystemMouseCursors.click,
+                    child: Text(
+                      'Create Account',
+                      style: TextStyle(
+                        decoration: TextDecoration.underline,
+                        color: Color.fromRGBO(123, 56, 255, 1),
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 50),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Wrap(
         spacing: 10,
         children: [
           Text(
@@ -24,8 +96,7 @@ class OnboardingView extends StatelessWidget {
             child: const Text('Sign In'),
           ),
           PrimaryButtonLarge(
-            onPressed: () =>
-                Get.toNamed(Routes.allPagesNav), //TODO: Change to register
+            onPressed: () => Get.toNamed(Routes.allPagesNav),
             child: const Text('Register'),
           ),
           SecondaryButtonMedium(
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,12 +1,15 @@
 import 'package:get/get.dart';
 import 'package:wismod/modules/auth/views/log_in_view.dart';
+import 'package:wismod/modules/auth/views/signup_view.dart';
 import 'package:wismod/modules/home/views/all_pages_nav.dart';
 import '../modules/home/views/onboarding.dart';
 
 abstract class Routes {
-  static const String allPagesNav = '/allPagesNav'; // Hold all 4 pages (Home, Chat, Dashboard, Settings with NavBar) so no need of seperate routes
+  static const String allPagesNav =
+      '/allPagesNav'; // Hold all 4 pages (Home, Chat, Dashboard, Settings with NavBar) so no need of seperate routes
   static const String onboarding = '/onboarding';
   static const String login = '/login';
+  static const String signup = '/signup';
   // Add page string (route) here
 }
 
@@ -14,5 +17,6 @@ final getPages = [
   GetPage(name: Routes.allPagesNav, page: () => AllPagesNav()),
   GetPage(name: Routes.onboarding, page: () => OnboardingView()),
   GetPage(name: Routes.login, page: () => const LogInView()),
+  GetPage(name: Routes.signup, page: () => const SignUpView()),
   // Initialize route here
 ];

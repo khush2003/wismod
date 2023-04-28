272495e7332871c1a288a70baaf1d0f3533b1161
Khush Agarwal <khush.agar@kmutt.ac.th>
2023-04-28 11:01:53 +0700

    Make onboarding UI responsive and fit actual mobile

--- lib/modules/home/views/onboarding.dart
+++ lib/modules/home/views/onboarding.dart
@@ -1,88 +1,71 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:wismod/utils/app_utils.dart';
 
 import '../../../routes/routes.dart';
 import '../../../theme/global_widgets.dart';
 
 class OnboardingView extends StatelessWidget {
-  OnboardingView({super.key});
-  final thc = Get.put(ThemedSwitchController());
-
+  const OnboardingView({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0.0, 60.0, 0.0, 25.0),
-                child: Text(
-                  'Welcome to',
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.displayLarge,
-                ),
-              ),
-            ],
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/images/WisModLonger.png',
-                height: 175,
-                fit: BoxFit.fill,
-              ),
-            ],
-          ),
-          const SizedBox(height: 50),
-          const Padding(
-            padding: EdgeInsets.fromLTRB(8.0, 70.0, 8.0, 110.0),
-            child: Text(
+      body: Padding(
+        padding: const EdgeInsets.all(sideWidth),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome to',
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            Image.asset(
+              'assets/images/WisModLonger.png',
+              height: 175,
+              fit: BoxFit.fill,
+            ),
+            const Text(
               'Join the community of KMUTT changemakers with WisMod',
               textAlign: TextAlign.center,
               style: TextStyle(fontSize: 24),
             ),
-          ),
-          const Expanded(
-            child: Text(""),
-          ),
-          PrimaryButtonMedium(
-            onPressed: () => Get.toNamed(Routes.login),
-            child: const Text('Log In'),
-          ),
-          const SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "Doesn't have an account? ",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 12),
-              ),
-              GestureDetector(
-                onTap: () {
-                  Get.toNamed('/signup');
-                },
-                child: const MouseRegion(
-                  cursor: SystemMouseCursors.click,
-                  child: Text(
-                    'Create Account',
-                    style: TextStyle(
-                      decoration: TextDecoration.underline,
-                      color: Color.fromRGBO(123, 56, 255, 1),
-                      fontSize: 12,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
+            Column(
+              children: [
+                PrimaryButtonMedium(
+                  onPressed: () => Get.toNamed(Routes.login),
+                  child: const Text('Log In'),
                 ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 50),
-        ],
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Don't have an account? ",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 12),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Get.toNamed(Routes.signup);
+                      },
+                      child: const Text(
+                        'Create Account',
+                        style: TextStyle(
+                          fontFamily: 'Gotham',
+                          decoration: TextDecoration.underline,
+                          color: Color.fromRGBO(123, 56, 255, 1),
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }

546aeaeba5e7994343e64c603bb86d539e6a06a1
Khush Agarwal <khush.agar@kmutt.ac.th>
2023-04-30 15:39:03 +0700

    Remove redundant code

--- lib/initial_bindings.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-// import 'package:wismod/modules/auth/controllers/auth_controller.dart';
-
-class InitialBinding extends Bindings {
-  @override
-  void dependencies() {
-    // Get.put(() => AuthController(), permanent: true);
-  }
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:wismod/firebase_options.dart';
-import 'package:wismod/initial_bindings.dart';
 import 'package:wismod/modules/auth/controllers/auth_controller.dart';
 import 'package:wismod/routes/routes.dart';
 import 'package:wismod/theme/theme_data.dart';
@@ -23,7 +22,6 @@ class Application extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'WisMod',
       theme: AppThemeData.themedata,
-      initialBinding: InitialBinding(),
       initialRoute: Routes.onboarding,
       getPages: getPages,
     );

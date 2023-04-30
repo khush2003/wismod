c6a87a4f13b70bdea3ee1d3bf921e0f4a3ac2f27
ImUlyssus <98200468+ImUlyssus@users.noreply.github.com>
2023-04-30 19:51:42 +0700

    Added SIT

--- lib/modules/auth/controllers/signup_controller.dart
+++ lib/modules/auth/controllers/signup_controller.dart
@@ -14,6 +14,7 @@ class SignUpController extends GetxController {
   final confirmPasswordController = TextEditingController();
 
   final departmentOptions = [
+    'School of Information Technology',
     'Department',
     'Department of Electrical Engineering',
     'Department of Computer Engineering',

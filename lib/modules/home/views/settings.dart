b5d1cb31365aee9bfd5e1d8be4ddcb890b5ee349
Khush Agarwal <khush.agar@kmutt.ac.th>
2023-04-30 15:57:31 +0700

    Settings initial

--- lib/modules/home/views/settings.dart
+++ lib/modules/home/views/settings.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:wismod/theme/global_widgets.dart';
+
+import '../../../routes/routes.dart';
 
 class SettingsView extends StatelessWidget {
   const SettingsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body:  Text("Settings"));
+    return Scaffold(
+      appBar: AppBar(title: const Text("Settings")),
+      body: SingleChildScrollView(
+          child: PrimaryButtonMedium(
+        child: const Text("Accounts"),
+        onPressed: () {
+          // Get.toNamed(Routes.accounts);
+          // Get.offAllNamed(Routes.accounts);
+          // Get.offNamed(Routes.accounts);
+        },
+      )),
+    );
   }
 }
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 import 'package:wismod/modules/auth/views/log_in_view.dart';
 import 'package:wismod/modules/auth/views/signup_view.dart';
+import 'package:wismod/modules/home/views/accounts_view.dart';
 import 'package:wismod/modules/home/views/all_pages_nav.dart';
 import '../modules/home/views/onboarding.dart';
 import '../modules/home/views/create_event.dart';
@@ -12,6 +13,7 @@ abstract class Routes {
   static const String login = '/login';
   static const String signup = '/signup';
   static const String createEvent = '/createEvent';
+  static const String accounts = '/accounts'; 
   // Add page string (route) here
 }
 
@@ -21,5 +23,6 @@ final getPages = [
   GetPage(name: Routes.login, page: () => LogInView()),
   GetPage(name: Routes.signup, page: () => SignUpView()),
   GetPage(name: Routes.createEvent, page: () => CreateEventView()),
+  GetPage(name: Routes.accounts, page: () => const AccountsView()),
   // Initialize route here
 ];

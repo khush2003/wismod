c0364b2f7939e2c52a181acc7167d255d0b17e72
JoNoteJoMaMa <93032309+JoNoteJoMaMa@users.noreply.github.com>
2023-05-15 01:13:38 +0700

    Year changing is done!
    User can change their year but it must be validate and not the same with the old one

--- lib/modules/home/controller/account_controller.dart
+++ lib/modules/home/controller/account_controller.dart
@@ -1,11 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:wismod/shared/services/firebase_firestore_serivce.dart';
 import 'package:wismod/utils/app_utils.dart';
-
-import '../../../shared/models/user.dart';
 import '../../auth/controllers/auth_controller.dart';
 
 class AccountController extends GetxController {
@@ -81,15 +80,6 @@ class AccountController extends GetxController {
   // }
 
   // Same name with old one check (experimental)
-  nameIstheSame(String newFirstname, newLastname) {
-    late String oldFirstname = _auth.appUser.value.getFirstname();
-    late String oldLasttname = _auth.appUser.value.getLastname();
-    if (newFirstname == oldFirstname && newLastname == oldLasttname) {
-      return true;
-    } else {
-      return false;
-    }
-  }
 
   late String preFirstname = _auth.appUser.value.getFirstname();
   late String preLastname = _auth.appUser.value.getLastname();
@@ -103,15 +93,44 @@ class AccountController extends GetxController {
   late TextEditingController yearController =
       TextEditingController(text: preYear.toString());
 
+  nameIsTheSame(String newFirstname, newLastname) {
+    late String oldFirstname = _auth.appUser.value.getFirstname();
+    late String oldLasttname = _auth.appUser.value.getLastname();
+    if (newFirstname == oldFirstname && newLastname == oldLasttname) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  validYearCheck(String? value) {
+    if (value == null || value.isEmpty) {
+      return false;
+    }
+    int? year = int.tryParse(value);
+    if (year == null || year == 0) {
+      return false;
+    }
+    if (year > 9) {
+      return false;
+    }
+    return true;
+  }
+
+  yearIsTheSame(int newYear) {
+    if (newYear == preYear) {
+      return true;
+    }
+    return false;
+  }
+
   Future<void> updateName() async {
     final List<String> splitedName = nameController.text.trim().split(' ');
-    // print(splitedName.toString());
 
-    // ignore: unnecessary_null_comparison
     if (splitedName.length == 2) {
       final String firstName = splitedName[0];
       final String lastName = splitedName[1];
-      if (nameIstheSame(firstName, lastName) == false) {
+      if (nameIsTheSame(firstName, lastName) == false) {
         try {
           final User? user = authBase.currentUser;
           final uid = user?.uid;
@@ -124,10 +143,12 @@ class AccountController extends GetxController {
               .collection("Users")
               .doc(uid)
               .update({'Lastname': lastName});
-          sucessSnackBar('Your Firstname and Lastname has been changed');
+          sucessSnackBar('Your first name and last name has been changed');
           authBase.currentUser!.reload();
         } catch (e) {
-          print('Error updating name: $e');
+          if (kDebugMode) {
+            print('Error updating name: $e');
+          }
         }
       } else {
         errorSnackBar(
@@ -136,6 +157,32 @@ class AccountController extends GetxController {
     } else {
       errorSnackBar(
           'Please fill in all the fields correctly to change your First name and Last name');
+    }
+  }
+
+  Future<void> updateYear() async {
+    String yearCheck = yearController.text;
+    if (validYearCheck(yearCheck)) {
+      int? year = int.tryParse(yearCheck);
+      if (yearIsTheSame(year!) == false) {
+        try {
+          final User? user = authBase.currentUser;
+          final uid = user?.uid;
+          await _fireBase.collection("Users").doc(uid).update({'Year': year});
+
+          sucessSnackBar('Your year has been changed');
+          authBase.currentUser!.reload();
+        } catch (e) {
+          if (kDebugMode) {
+            print('Error updating name: $e');
+          }
+        }
+      } else {
+        errorSnackBar(
+            "You can't fill new year that's the same with the old one");
+      }
+    } else {
+      errorSnackBar('Please fill correct year');
     }
   }
 }
--- lib/modules/home/views/accounts.dart
+++ lib/modules/home/views/accounts.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
@@ -84,7 +83,7 @@ class AccountsView extends StatelessWidget {
                     ),
                   ),
                   onPressed: () {
-                    //
+                    accountController.updateYear();
                   },
                 ),
               ),
